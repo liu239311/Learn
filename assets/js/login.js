1663550bfdcfb46e70162d7652f429eb9a2f5638
liu239311 <2393112912@qq.com>
2020-09-08 20:29:02 +0800

    注册功能完成

--- assets/js/login.js
+++ assets/js/login.js
@@ -2,24 +2,43 @@ $(function () {
   $('.login a').click(function () {
     $('.login').hide().next().show()
   })
-  // 点击去登录
   $('.register a').click(function () {
     $('.register').hide().prev().show()
   })
-  // // console.log($('.register form'));
-  $('.register form').submit(function (e) {
-    // e.preventDefault()
-    var data = {
-      username: $('.register form input[name=username]'),
-      password: $('.register form input[name=password]')
+  var form = layui.form
+  form.verify({
+    password: function (value) {
+      // value表示使用验证规则的输入框的值
+      if (!/^\S{6,12}$/.test(value)) {
+        return '密码长度必须是6~12位，并且不能有空格'
+      }
+    },
+    // 验证两次密码
+    regpassword: function (value) {
+      // value 表示确认密码
+      let pwd = $('input[name="password"]').val().trim() // 获取密码
+      if (value !== pwd) {
+        return '两次密码不一致'
+      }
+    },
+  })
+});
+// 点击去登录
+
+$('.register form').submit(function (e) {
+  e.preventDefault();
+  var data = {
+    username: $('.register form input[name=username]').val(),
+    password: $('.register form input[name=password]').val()
+  }
+  $.post('http://ajax.frontend.itheima.net/api/reguser', data, function (
+    res
+  ) {
+    if (res.status === 0) {
+      console.log(res.message)
+    } else {
+      console.log(res.message)
     }
-    $.ajax({
-      type: 'post',
-      url: 'http://ajax.frontend.itheima.net/api/reguser',
-      data,
-      success: function (res) {
-        console.log(res);
-      },
-    })
+    alert(1)
   })
 })
